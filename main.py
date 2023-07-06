5e51dd35479491354060893fdbb8d0cc926695ff
rtr <nitin@gmail.com>
2023-07-06 16:20:39 +0530

    added serial number colums to the csv file

--- main.py
+++ main.py
@@ -13,9 +13,20 @@ print(ds.shape)
 eg=ds['review'][50]
 
 tokens = nltk.word_tokenize(eg)
-print(tokens[:10])
+print(tokens)
 tagged=nltk.pos_tag(tokens[:10])
 print(tagged)
 entities = nltk.chunk.ne_chunk(tagged)
 entities.pprint()
 
+from nltk.sentiment import SentimentIntensityAnalyzer
+from tqdm.notebook import tqdm
+
+sia = SentimentIntensityAnalyzer()
+
+print(sia.polarity_scores(eg))
+
+res={}
+for i,row in tqdm (ds.iterrows(), total=len(ds));
+    text= row['review']
+    myid = row['s/n']
